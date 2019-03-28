19f1edd18df6f42a162ad39da282220219b252ab
Kevin Oglesby <kevinjoglesby@gmail.com>
2019-03-28 14:04:55 -0700

    updating posts on server

--- backend/app.js
+++ backend/app.js
@@ -61,7 +61,6 @@ app.put('/api/posts/:id', (req, res, next) => {
     content: req.body.content
   });
   Post.updateOne({ _id: req.params.id }, post).then(result => {
-    console.log(result);
     res.status(200).json({ message: 'Update Successful' });
   });
 });
@@ -75,10 +74,20 @@ app.get('/api/posts', (req, res, next) => {
   });
 });
 
+app.get('/api/posts/:id', (req, res, next) => {
+  Post.findById(req.params.id).then(post => {
+    if (post) {
+      res.status(200).json(post);
+    } else {
+      res.status(404).json({ message: 'post not found' });
+    }
+  });
+});
+
 app.delete('/api/posts/:id', (req, res, next) => {
   // console.log(req.params.id);
   Post.deleteOne({ _id: req.params.id }).then(result => {
-    console.log(result);
+    // console.log(result);
     res.status(200).json({ message: 'post deleted' });
   });
 });
