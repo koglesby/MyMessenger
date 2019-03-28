aed084e9f0060faa22ce0cdfdd2f203360cbfb56
Kevin Oglesby <kevinjoglesby@gmail.com>
2019-03-27 17:36:20 -0700

    post edit feature

--- backend/app.js
+++ backend/app.js
@@ -1,32 +1,45 @@
 const express = require('express');
-const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 const testCredentials = require('../testCredentials');
 
 const Post = require('./models/post');
 
 const app = express();
 
-mongoose.connect("mongodb://" + testCredentials.username + ":" + testCredentials.password + "@ds221258.mlab.com:21258/mean-messenger-2").then(() => {
-  console.log("connected to mongodb");
-})
+mongoose
+  .connect(
+    'mongodb://' +
+      testCredentials.username +
+      ':' +
+      testCredentials.password +
+      '@ds221258.mlab.com:21258/mean-messenger-2'
+  )
+  .then(() => {
+    console.log('connected to mongodb');
+  })
   .catch(() => {
-    console.log("connection failed");
+    console.log('connection failed');
   });
-
 
 // Middleware
 
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE, OPTIONS");
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PATCH, PUT, DELETE, OPTIONS'
+  );
   next();
 });
 
-app.post("/api/posts", (req, res, next) => {
+app.post('/api/posts', (req, res, next) => {
   const post = new Post({
     title: req.body.title,
     content: req.body.content
@@ -41,6 +54,17 @@ app.post("/api/posts", (req, res, next) => {
   });
 });
 
+app.put('/api/posts/:id', (req, res, next) => {
+  const post = new Post({
+    _id: req.body.id,
+    title: req.body.title,
+    content: req.body.content
+  });
+  Post.updateOne({ _id: req.params.id }, post).then(result => {
+    console.log(result);
+    res.status(200).json({ message: 'Update Successful' });
+  });
+});
 
 app.get('/api/posts', (req, res, next) => {
   Post.find().then(documents => {
@@ -51,11 +75,11 @@ app.get('/api/posts', (req, res, next) => {
   });
 });
 
-app.delete("/api/posts/:id", (req, res, next) => {
+app.delete('/api/posts/:id', (req, res, next) => {
   // console.log(req.params.id);
   Post.deleteOne({ _id: req.params.id }).then(result => {
     console.log(result);
-    res.status(200).json({ message: "post deleted" });
+    res.status(200).json({ message: 'post deleted' });
   });
 });
 
--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -1,29 +1,32 @@
-import { Post } from "./post.model";
-import { Injectable } from "@angular/core";
-import { Subject } from "rxjs";
+import { Post } from './post.model';
+import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
 
 // provides the service at the root level, creating only 1 instance for the entire app.
-@Injectable({ providedIn: "root" })
+@Injectable({ providedIn: 'root' })
 export class PostsService {
   private posts: Post[] = [];
   private postsUpdated = new Subject<Post[]>();
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   getPosts() {
-    this.http.get<{ message: string, posts: any }>('http://localhost:3000/api/posts')
-      .pipe(map((postData) => {
-        return postData.posts.map(post => {
-          return {
-            title: post.title,
-            content: post.content,
-            id: post._id
-          };
-        });
-      }))
-      .subscribe((transformedPosts) => {
+    this.http
+      .get<{ message: string; posts: any }>('http://localhost:3000/api/posts')
+      .pipe(
+        map(postData => {
+          return postData.posts.map(post => {
+            return {
+              title: post.title,
+              content: post.content,
+              id: post._id
+            };
+          });
+        })
+      )
+      .subscribe(transformedPosts => {
         this.posts = transformedPosts;
         this.postsUpdated.next([...this.posts]);
       });
@@ -34,18 +37,35 @@ export class PostsService {
     return this.postsUpdated.asObservable();
   }
 
+  getPost(id: string) {
+    return { ...this.posts.find(p => p.id === id) };
+  }
+
   addPost(title: string, content: string) {
     const post: Post = { id: null, title, content };
-    this.http.post<{ message: string, postId: string }>('http://localhost:3000/api/posts', post).subscribe((responseData) => {
-      const postId = responseData.postId;
-      post.id = postId;
-      this.posts.push(post);
-      this.postsUpdated.next([...this.posts]);
-    });
+    this.http
+      .post<{ message: string; postId: string }>(
+        'http://localhost:3000/api/posts',
+        post
+      )
+      .subscribe(responseData => {
+        const postId = responseData.postId;
+        post.id = postId;
+        this.posts.push(post);
+        this.postsUpdated.next([...this.posts]);
+      });
+  }
+
+  updatePost(id: string, title: string, content: string) {
+    const post: Post = { id, title, content };
+    this.http
+      .put('http://localhost:3000/api/posts/' + id, post)
+      .subscribe(response => console.log(response));
   }
 
   deletePost(postId: string) {
-    this.http.delete("http://localhost:3000/api/posts/" + postId)
+    this.http
+      .delete('http://localhost:3000/api/posts/' + postId)
       .subscribe(() => {
         const updatedPosts = this.posts.filter(post => post.id !== postId);
         this.posts = updatedPosts;
