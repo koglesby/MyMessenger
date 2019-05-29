3af9d79f8d15fd56a262e8700a5a1742a557d325
Kevin Oglesby <kevinjoglesby@gmail.com>
2019-05-29 13:53:52 -0700

    send token to frontend

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const User = require('../models/user');
 
@@ -25,6 +26,44 @@ router.post('/signup', (req, res, next) => {
         });
       });
   });
+});
+
+router.post('/login', (req, res, next) => {
+  let fetchedUser;
+  User.findOne({ email: req.body.email })
+    .then(user => {
+      console.log('user: ', user);
+      if (!user) {
+        return res.status(401).json({
+          message: 'auth failed, !user'
+        });
+      }
+      fetchedUser = user;
+
+      return bcrypt.compare(req.body.password, user.password);
+    })
+    .then(result => {
+      console.log('result', result);
+      if (!result) {
+        return res.status(401).json({
+          message: 'auth failed, !result'
+        });
+      }
+      const token = jwt.sign(
+        { email: fetchedUser.email, userId: fetchedUser._id },
+        'secret_for_development',
+        { expiresIn: '1h' }
+      );
+      res.status(200).json({
+        token
+      });
+    })
+    .catch(err => {
+      return res.status(401).json({
+        message: 'auth failed, err',
+        err
+      });
+    });
 });
 
 module.exports = router;
